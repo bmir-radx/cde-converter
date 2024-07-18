ac7a6309f050ad148df9c4750eb6ea6d23a110c1
jkyu <jkyu@stanford.edu>
2024-07-18 13:09:01 -0700

    removed old function signature

--- src/main/kotlin/edu.stanford.bmir.radx.cde.generator/Parser.kt
+++ src/main/kotlin/edu.stanford.bmir.radx.cde.generator/Parser.kt
@@ -3,6 +3,5 @@ package edu.stanford.bmir.radx.cde.generator
 import java.nio.file.Path
 
 abstract class Parser {
-//    abstract fun dataframeToDataElements(df: List<Map<String, Any>>): List<DataElement>
     abstract fun fileToDataElements(fileName: Path): List<DataElement>
 }
