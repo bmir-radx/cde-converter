5cd13409c2165eaa9baff5f98d890465871c8922
jkyu <jkyu@stanford.edu>
2024-08-07 11:56:08 -0700

    updates to current cedar-artifact-library API

--- src/main/kotlin/edu.stanford.bmir.radx.cde.generator/Converter.kt
+++ src/main/kotlin/edu.stanford.bmir.radx.cde.generator/Converter.kt
@@ -1,18 +1,17 @@
 package edu.stanford.bmir.radx.cde.generator
 
-import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.node.ObjectNode
 import org.metadatacenter.artifacts.model.core.FieldSchemaArtifact
-import org.metadatacenter.artifacts.model.core.NumericType
-import org.metadatacenter.artifacts.model.core.builders.FieldSchemaArtifactBuilder
-import org.metadatacenter.artifacts.model.core.builders.ListFieldBuilder
+import org.metadatacenter.artifacts.model.core.fields.XsdNumericDatatype
+import org.metadatacenter.artifacts.model.core.NumericField.NumericFieldBuilder
+import org.metadatacenter.artifacts.model.core.TextField.TextFieldBuilder
+import org.metadatacenter.artifacts.model.core.ListField.ListFieldBuilder
 import org.metadatacenter.artifacts.model.renderer.JsonSchemaArtifactRenderer
 import org.springframework.stereotype.Component
 
 @Component
 class Converter {
 
-    private val objectMapper = ObjectMapper()
     private val renderer = JsonSchemaArtifactRenderer()
 
     fun convert(cdes: List<DataElement>): List<ObjectNode> {
@@ -25,30 +24,31 @@ class Converter {
     }
 
     private fun makeCedarArtifact(dataElement: DataElement): FieldSchemaArtifact {
-        val builder = chooseBuilder(dataElement.inputType, dataElement.values)
-        builder.withIdentifier(dataElement.id)
-        builder.withName(dataElement.label)
-        builder.withPreferredLabel(dataElement.prefLabel)
+        val builder = when (dataElement.inputType) {
+            "Text" -> TextFieldBuilder()
+                .withIdentifier(dataElement.id)
+                .withName(dataElement.label)
+                .withPreferredLabel(dataElement.prefLabel)
+            "Number" -> NumericFieldBuilder()
+                .withNumericType(XsdNumericDatatype.INTEGER)
+                .withIdentifier(dataElement.id)
+                .withName(dataElement.label)
+                .withPreferredLabel(dataElement.prefLabel)
+            "Value List" -> getListFieldBuilder(dataElement.values)
+                .withIdentifier(dataElement.id)
+                .withName(dataElement.label)
+                .withPreferredLabel(dataElement.prefLabel)
+            else -> ListFieldBuilder() // Throw an exception
+        }
         return builder.build()
     }
 
-    private fun chooseBuilder(inputType: String, values: List<Value>): FieldSchemaArtifactBuilder {
-        val builder = when (inputType) {
-            "Text" -> FieldSchemaArtifact.textFieldBuilder()
-            "Number" -> FieldSchemaArtifact.numericFieldBuilder()
-                    .withNumericType(NumericType.INTEGER)
-            "Value List" -> getListFieldBuilder(values)
-            else -> FieldSchemaArtifact.listFieldBuilder() // Throw an exception
-        }
-        return builder
-    }
-
     private fun getListFieldBuilder(values: List<Value>): ListFieldBuilder {
-        val builder = FieldSchemaArtifact.listFieldBuilder()
+        val builder = ListFieldBuilder()
         for (value in values) {
             builder.withOption(value.toString())
         }
-        builder.withMultipleChoice(false)
+        builder.withIsMultiple(false)
         return builder
     }
 }
